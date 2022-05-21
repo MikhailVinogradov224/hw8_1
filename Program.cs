e22d925879167b95cacdd3659734e67faab7c821
Mikhail Vinogradov <m89119750476@gmail.com>
2022-05-22 00:37:14 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+ 
+namespace hw8_1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var a = new Int32[4, 5];
+ 
+            var random = new Random();
+            for (var i = 0; i < a.GetLength(0); i++, Console.WriteLine())
+            {
+                for (var j = 0; j < a.GetLength(1); j++)
+                {
+                    a[i, j] = random.Next(100);
+                    Console.Write("{0,4}", a[i, j]);
+                }
+            }
+            
+            Console.WriteLine(" ");
+            
+            for (var i = 0; i < a.GetLength(0); i++)
+                
+                for (var j = 0; j < a.GetLength(1); j++)
+                    for (var k = 0; k < a.GetLength(1); k++)
+                    {
+                        if (a[i, j] <= a[i, k]) continue;
+                        var temp = a[i, j];
+                        a[i, j] = a[i, k];
+                        a[i, k] = temp;
+                    }
+ 
+            for (var i = 0; i < a.GetLength(0); i++, Console.WriteLine())
+                for (var j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write("{0,4}", a[i, j]);
+                }
+ 
+            
+            Console.Read();
+        }}} 
+                    
+                
+            
+        
+    
